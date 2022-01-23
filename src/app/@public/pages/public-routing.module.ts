ede5983dde89bcf7177eda9ed06bc61558b47622
abraham casas <abrahamcomservice@gmail.com>
2022-01-23 16:02:24 -0600

    03-Plantilla base de la tinenda terminada

--- src/app/@public/pages/public-routing.module.ts
+++ src/app/@public/pages/public-routing.module.ts
@@ -2,29 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PublicComponent } from './public.component';
 
-
 const routes: Routes = [
   {
     path: '',
     component: PublicComponent,
     children: [
       {
-        
-          path: 'home',
-          loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-        },
-        {
-          path: 'contact',
-          loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
-        },
-      
-    ]
-
-  }
+        path: '',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'contact',
+        loadChildren: () =>
+          import('./contact/contact.module').then((m) => m.ContactModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PublicRoutingModule { }
+export class PublicRoutingModule {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,27 +2,26 @@ import { HomeModule } from './@public/pages/home/home.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    redirectTo: '',
+    pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  }
-  
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,
-    {
+  imports: [
+    RouterModule.forRoot(routes, {
       useHash: true,
-      scrollPositionRestoration: 'enabled'
-    })],
-  exports: [RouterModule]
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
